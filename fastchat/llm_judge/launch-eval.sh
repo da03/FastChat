ed2da1462b45f1c594647285e784103ef2a922b1
Wenting Zhao <wentingz@mosaic-cirrascale-35.reviz.ai2.in>
2023-09-08 07:08:54 -0700

    Updates eval script

--- fastchat/llm_judge/launch-eval.sh
+++ fastchat/llm_judge/launch-eval.sh
@@ -3,3 +3,5 @@ ID=$2
 CUDA_VISIBLE_DEVICES=7 python gen_model_answer.py --model-path $MODEL --model-id $ID
 python gen_judgment.py --model-list $ID --mode pairwise-baseline --baseline-model vicuna-7b-v1.5
 python gen_judgment.py --model-list $ID 
+python show_result.py --model-list vicuna-7b-v1.5 $ID
+python show_result.py --mode pairwise-baseline --model-list $ID --baseline-model vicuna-7b-v1.5
